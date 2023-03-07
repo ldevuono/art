1883ae784681211d4b05aef205c8cae0f4db0766
Laura De Vuono <101818803+ldevuono@users.noreply.github.com>
2023-03-07 15:44:16 -0500

    initial skeleton + initial hardcoded api call

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("hello")
+
+
+fetch('https://api.artic.edu/api/v1/artworks/27992?fields=id,title,image_id')
+    .then(function (res) {
+        return res.json()
+    })
+    .then(function (jsonRes) {
+        console.log(jsonRes)
+    })
