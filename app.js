6b759eba5b9607ebedbd5e21af83e8459f969fb7
Laura De Vuono <lauradevuono@Lauras-MacBook-Air.local>
2023-03-28 20:48:01 -0400

    getting art on page

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const artApp = {};
 
 const input = "chicken";
 artApp.getArt = function () {
-    const url = new URL(`https://api.artic.edu/api/v1/artworks/search?`)
+    const url = new URL(`https://api.artic.edu/api/v1/artworks?`)
     url.search = new URLSearchParams({
         q: input
     })
@@ -11,10 +11,33 @@ artApp.getArt = function () {
             return res.json()
         })
         .then(function (jsonRes) {
-            console.log(jsonRes.data)
+            artApp.displayArt(jsonRes.data);
+            console.log(jsonRes.data);
         })
 };
 
+artApp.displayArt = function (artArray) {
+    artArray.forEach(function (artPiece) {
+        const title = document.createElement("h2");
+        title.innerText = artPiece.title;
+
+        const artist = document.createElement("h3");
+        artist.innerText = artPiece.artist_title;
+
+        const image = document.createElement("img");
+        image.src = `https://www.artic.edu/iiif/2/${artPiece.image_id}/full/843,/0/default.jpg`;
+        image.alt = artPiece.title;
+
+        const artBox = document.createElement("div");
+        artBox.classList.add('artBox');
+
+        artBox.appendChild(title);
+        artBox.appendChild(artist);
+        artBox.appendChild(image);
+
+        document.querySelector("#artwork").appendChild(artBox)
+    })
+}
 
 
 artApp.init = function () {
